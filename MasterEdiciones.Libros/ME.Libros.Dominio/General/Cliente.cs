8d5107b14f0dac149ee76176c80cafa1a72fdb9a
ernestomangia <ernesto_mangia@hotmail.com>
2015-01-06 15:03:48 -0300

    Crear arquitectura

---
+++ MasterEdiciones.Libros/ME.Libros.Dominio/General/Cliente.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Dominio.General
+{
+    public class Cliente
+    {
+        #region Properties
+
+        public virtual string Nombre { set; get; }
+        public virtual string Apellido { set; get; }
+        public virtual string Identificacion { set; get; }
+        public virtual string FechaNacimiento { set; get; }
+
+        #endregion
+    }
+}
---
+++ MasterEdiciones.Libros/ME.Libros.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ME.Libros.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Hoem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

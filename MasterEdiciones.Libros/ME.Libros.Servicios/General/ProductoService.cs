06704591b20757c1951bc86986a4726094e16112
denisomar <denisomar@hotmail.com.ar>
2015-04-07 16:19:21 -0300

    Crear, Index, Rubro

---
+++ MasterEdiciones.Libros/ME.Libros.Servicios/General/ProductoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ME.Libros.Api.Repositorios;
+using ME.Libros.Dominio.General;
+
+namespace ME.Libros.Servicios.General
+{
+    public class ProductoService:AbstractService<ProductoDominio>
+    {
+        public ProductoService(IRepository<ProductoDominio> repository)
+            : base(repository)
+        {
+            
+        }
+    }
+}

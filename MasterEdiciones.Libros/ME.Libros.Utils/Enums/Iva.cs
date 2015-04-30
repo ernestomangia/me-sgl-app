a907d1539cc901c00e20d2b9e55470f1387591fe
denisomar <denisomar@hotmail.com.ar>
2015-04-30 20:33:12 -0300

    Agregar - iva

---
+++ MasterEdiciones.Libros/ME.Libros.Utils/Enums/Iva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME.Libros.Utils.Enums
+{
+    public enum Iva
+    {
+        ConsumidorFinal = 1,
+        Monotributo = 2,
+        ResponsableInscripto = 3,
+        Exento = 4,
+        NoInscripto = 5,
+
+    };
+}

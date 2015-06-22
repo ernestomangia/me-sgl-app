c349dffe35cacf29b837dafb3971eea2c18b2510
ernestomangia <ernesto_mangia@hotmail.com>
2015-06-22 13:40:30 -0300

    Agregar Cuota dominio

---
+++ MasterEdiciones.Libros/ME.Libros.Dominio/General/CuotaDominio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ME.Libros.Utils.Enums;
+
+namespace ME.Libros.Dominio.General
+{
+    class CuotaDominio : BaseDominio
+    {
+        #region Properties
+
+        public virtual DateTime Periodo { get; set; }
+        public virtual int Numero { get; set; }
+        public virtual EstadoCuota Estado { get; set; }
+        public virtual DateTime FechaVencimiento { get; set; }
+        public virtual decimal Monto { get; set; }
+        public virtual decimal Interes { get; set; }
+        public virtual VentaDominio Venta { get; set; }
+
+        #endregion
+    }
+}

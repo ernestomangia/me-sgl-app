a25457c808556e52257475bbbc010313300ad933
franssgti <fran_310188@hotmail.com>
2015-07-13 18:58:13 -0300

    ABM Iva

---
+++ MasterEdiciones.Libros/ME.Libros.Dominio/General/IvaDominio.cs
@@ -0,0 +1,12 @@
+﻿namespace ME.Libros.Dominio.General
+{
+    public class IvaDominio : BaseDominio
+    {
+        #region Properties
+
+        public virtual string Nombre { get; set; }
+        public virtual decimal Alicuota { get; set; }
+
+        #endregion
+    }
+}

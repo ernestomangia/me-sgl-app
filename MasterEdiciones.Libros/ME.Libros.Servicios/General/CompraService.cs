d6b35f0aed378747e4724735a3bdbebf940cca29
denisomar <denisomar@hotmail.com.ar>
2015-08-03 20:57:39 -0300

    compra- arreglos, anular, sumar stock.

--- MasterEdiciones.Libros/ME.Libros.Servicios/General/CompraService.cs
+++ MasterEdiciones.Libros/ME.Libros.Servicios/General/CompraService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ME.Libros.Api.Repositorios;
 using ME.Libros.Dominio.General;
+using ME.Libros.Utils.Enums;
 
 namespace ME.Libros.Servicios.General
 {
@@ -23,6 +24,61 @@ namespace ME.Libros.Servicios.General
         {
             ProductoService = productoService;
         }
+
+             public override long Guardar(CompraDominio compraDominio)
+             {
+                 if (compraDominio.Id == 0)
+                 {
+                     if (!Validar(compraDominio))
+                     {
+                         return -1;
+                     }
+
+                     foreach (var compraItemDominio in compraDominio.CompraItems)
+                     {
+                         var producto = compraItemDominio.Producto;
+                         ProductoService.SumarStock(producto, compraItemDominio.Cantidad);
+                         compraItemDominio.PrecioCosto = producto.PrecioCosto;
+                         compraItemDominio.PrecioCompraCalculado = producto.PrecioVenta;
+                         CalcularTotalItem(compraItemDominio);
+                     }
+
+                    
+                     CalcularTotalCompra(compraDominio);
+                 }
+
+                 return base.Guardar(compraDominio);
+             }
+
+
+             public long AnularCompra(long id)
+             {
+                 var compraDominio = GetPorId(id);
+                 if (compraDominio.Estado == EstadoCompra.Pagada)
+                 {
+                     compraDominio.Estado = EstadoCompra.Anulada;
+                     foreach (var compraItemDominio in compraDominio.CompraItems)
+                     {
+                         ProductoService.RestarStock(compraItemDominio.Producto, compraItemDominio.Cantidad);
+                     }
+                 }
+
+                 return Guardar(compraDominio);
+             }
+
+
+             #region Private Methods
+
+             private void CalcularTotalItem(CompraItemDominio compraItemDominio)
+             {
+                 compraItemDominio.MontoCalculado = compraItemDominio.Cantidad * compraItemDominio.PrecioCompraCalculado;
+             }
+
+             private void CalcularTotalCompra(CompraDominio entidad)
+             {
+                 entidad.MontoCalculado = entidad.CompraItems.Sum(vi => vi.MontoComprado);
+             }
+             #endregion
 
     }
 

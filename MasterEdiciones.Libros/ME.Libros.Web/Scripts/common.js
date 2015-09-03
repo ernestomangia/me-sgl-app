387545c65022f66889cc8787ed266be62ccad305
ernestomangia <ernesto_mangia@hotmail.com>
2015-09-03 15:41:34 -0300

    Update Datatable.net. Agregar row detail en cuotas.

--- MasterEdiciones.Libros/ME.Libros.Web/Scripts/common.js
+++ MasterEdiciones.Libros/ME.Libros.Web/Scripts/common.js
@@ -226,3 +226,15 @@ function getProductoByCodigoBarra(codigoBarra) {
         $("#precioSugerido").text("-");
     }
 }
+
+$(function () {
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    $('.nav-tabs a').click(function (e) {
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+});

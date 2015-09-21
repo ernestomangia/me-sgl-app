18637a8cfb0ff79315db7cf93e38675aa87ddd13
denisomar <denisomar@hotmail.com.ar>
2015-09-21 19:29:00 -0300

    Planilla Cobradores

--- MasterEdiciones.Libros/ME.Libros.Web/Controllers/ReporteController.cs
+++ MasterEdiciones.Libros/ME.Libros.Web/Controllers/ReporteController.cs
@@ -7,6 +7,7 @@ using ME.Libros.Dominio.General;
 using ME.Libros.EF;
 using ME.Libros.Repositorios;
 using ME.Libros.Servicios.General;
+using ME.Libros.Utils.Enums;
 using ME.Libros.Web.Models;
 using Rotativa;
 using Rotativa.Options;
@@ -16,11 +17,14 @@ namespace ME.Libros.Web.Controllers
     public class ReporteController : Controller
     {
         public ClienteService ClienteService { get; set; }
+        public VentaService VentaService { get; set; }
+
 
         public ReporteController()
         {
             var modelContainer = new ModelContainer();
             ClienteService = new ClienteService(new EntidadRepository<ClienteDominio>(modelContainer));
+            VentaService = new VentaService(new EntidadRepository<VentaDominio>(modelContainer));
             ViewBag.Title = "Reportes";
         }
 
@@ -60,5 +64,46 @@ namespace ME.Libros.Web.Controllers
             }
             return View(clientes);
         }
+
+            public ActionResult PlanillaCobrador()
+            {
+                var footer = "--footer-right \"[date] [time]\" " +
+                             "--footer-line --footer-font-size \"8\" " +
+                             "--footer-font-name \"calibri light\""+
+                             "--header-left='[webpage]'";
+                return new ActionAsPdf("PlanillaCobradorPDF")
+                {   
+                    PageOrientation = Orientation.Portrait,
+                    PageSize = Size.A4,
+                    PageMargins = new Margins(5,10,10,10),
+                    CustomSwitches = "--print-media-type " + footer,
+                };
+            }
+
+            public ActionResult PlanillaCobradorPDF()
+            {
+                var planillas = new List<PlanillaCobradorViewModel>();
+                using (VentaService)
+                {
+                    planillas.AddRange(VentaService.ListarAsQueryable()
+                        .Where(v => v.Estado == EstadoVenta.Vigente)
+                        .GroupBy(v => new { v.Cobrador, v.Cliente.Localidad },
+                            v => v,
+                            (key, group) => new
+                            {
+                                key.Cobrador,
+                                key.Localidad,
+                                Ventas = group.OrderBy(v => v.FechaVenta)
+                            })
+                        .OrderBy(p => p.Cobrador.Apellido)
+                        .ThenBy(p => p.Cobrador.Nombre)
+                        .ThenBy(p => p.Localidad.Nombre)
+                        .ToList()
+                        .Select(p => new PlanillaCobradorViewModel(p.Cobrador, p.Localidad, p.Ventas)));
+                }
+
+                return View(planillas);
+            }
+
     }
 }

3ff336ef22223c6e765b77f4eda471f0f433afea
denisomar <denisomar@hotmail.com.ar>
2015-09-21 23:02:30 -0300

    planilla cobrador

---
+++ MasterEdiciones.Libros/ME.Libros.Web/Models/PlanillaCobradorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ME.Libros.Dominio.General;
+
+namespace ME.Libros.Web.Models
+{
+    public class PlanillaCobradorViewModel
+    {
+        public PlanillaCobradorViewModel()
+        {
+            
+        }
+
+        public PlanillaCobradorViewModel(CobradorDominio cobrador, LocalidadDominio localidad,
+            IEnumerable<VentaDominio> ventas)
+        {
+            Cobrador = new CobradorViewModel(cobrador);
+            Localidad=new LocalidadViewModel(localidad);
+            Ventas = new List<VentaViewModel>(ventas.Select(v => new VentaViewModel(v)));
+
+        }
+
+        public CobradorViewModel Cobrador { get; set; }
+        public LocalidadViewModel Localidad { get; set; }
+        public List<VentaViewModel> Ventas { get; set; }
+
+
+    }
+
+
+}

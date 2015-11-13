6d1bd30f16bbc371f09af89f85475c7712991752
ernestomangia <ernesto_mangia@hotmail.com>
2015-11-13 16:12:54 -0300

    Crear pantalla de reporte (primer diseño) Agregar menu lateral a la pantalla de reporte Agregar filtros a la vista

---
+++ MasterEdiciones.Libros/ME.Libros.Web/Models/ReporteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using ME.Libros.Utils.Enums;
+
+namespace ME.Libros.Web.Models
+{
+    public class ReporteViewModel
+    {
+        public ReporteViewModel()
+        {
+        }
+
+        public long VentaId { get; set; }
+        public string Cliente { get; set; }
+        public string Cobrador { get; set; }
+        public string Vendedor { get; set; }
+        public EstadoVenta? EstadoVenta { get; set; }
+        public DateTime? Desde { get; set; }
+        public DateTime? Hasta { get; set; }
+    }
+}
